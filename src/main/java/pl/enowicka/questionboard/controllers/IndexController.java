f826d342831aa1639238386e007ff6585915c9fb
Ewa Nowicka <ewa.nowicka@gmail.com>
2019-11-30 12:19:00 +0100

    basic index dispaly

---
+++ src/main/java/pl/enowicka/questionboard/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package pl.enowicka.questionboard.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+
+}
