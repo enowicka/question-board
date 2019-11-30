8ff40409328657779379f1fcf6825bc9b9bbd8b5
Ewa Nowicka <ewa.nowicka@gmail.com>
2019-11-30 11:40:54 +0100

    init

---
+++ src/main/java/pl/enowicka/questionboard/QuestionBoardApplication.java
@@ -0,0 +1,13 @@
+package pl.enowicka.questionboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/enowicka/questionboard/QuestionBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.enowicka.questionboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
