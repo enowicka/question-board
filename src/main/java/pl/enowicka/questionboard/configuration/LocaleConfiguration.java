c11ba125d0d271f8a6becec8814c77e44f893a5a
Ewa Nowicka <ewa.nowicka@gmail.com>
2019-11-30 13:53:40 +0100

    added spring security dependecy added home domain

---
+++ src/main/java/pl/enowicka/questionboard/configuration/LocaleConfiguration.java
@@ -0,0 +1,21 @@
+package pl.enowicka.questionboard.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfiguration {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.forLanguageTag("pl"));
+        return slr;
+    }
+
+}
+
