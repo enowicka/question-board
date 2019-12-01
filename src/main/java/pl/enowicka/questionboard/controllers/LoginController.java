0b07c872454aaa4de4954445b4f7808898bcfcee
Ewa Nowicka <ewa.nowicka@gmail.com>
2019-12-01 10:31:35 +0100

    Login.html Solved the loop by adding csrf Fixed main page link

--- src/main/java/pl/enowicka/questionboard/controllers/LoginController.java
+++ src/main/java/pl/enowicka/questionboard/controllers/LoginController.java
@@ -1,14 +1,17 @@
 package pl.enowicka.questionboard.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String getLogin() {
+    public String getLogin(Model model, @RequestParam(defaultValue = "false") boolean error) {
+        model.addAttribute("error", error);
         return "login";
     }
 
